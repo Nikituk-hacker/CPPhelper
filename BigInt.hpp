d8c2af49ee898c43c60823aea17f2ecd0fd2c582
justlm <73307724+0n15xjstlm@users.noreply.github.com>
2024-01-18 00:22:53 +0300

    added multiplication + minor useful func-s
    exactly i added signum, max, abs, and i think that's all. multiplication should work with any number and any sign.

--- BigInt.hpp
+++ BigInt.hpp
@@ -242,22 +242,79 @@ bi operator-(bi a, bi b) {
     }
     bi ans;
     ans.num = "";
-    if (a.size() > b.size()) {
-        b.num = std::string(a.size() - b.size(), '0') + b.num;
+    if (a.num.size() > b.num.size()) {
+        b.num = std::string(a.num.size() - b.num.size(), '0') + b.num;
     }
-    for (int i = a.size() - 1; i >= 0; --i) {
+    for (int i = a.num.size() - 1; i >= 0; --i) {
         if (a.num[i] >= b.num[i]) {
-            ans.push_back((char)a.num[i] - b.num[i] + '0');
+            ans.num.push_back((char)a.num[i] - b.num[i] + '0');
         } else {
             a.num[i - 1] -= 1;
-            ans.push_back((char)a.num[i] + 10 - b.num[i] + '0');
+            ans.num.push_back((char)a.num[i] + 10 - b.num[i] + '0');
         }
     }
-    std::reverse(ans.begin(), ans.end());
-    while (ans.size() > 1 && *ans.begin() == '0') {
-        ans.erase(ans.begin());
+    std::reverse(ans.num.begin(), ans.num.end());
+    while (ans.num.size() > 1 && *ans.num.begin() == '0') {
+        ans.num.erase(ans.num.begin());
     }
     return ans;
+}
+
+long long binpow(long long a, long long b) {
+    if (b == 0) {
+        return 1;
+    } 
+    if (b % 2) {
+        return binpow(a, b - 1) * a;
+    }
+    long long tmp = binpow(a, b / 2);
+    return tmp * tmp;
+}
+
+bi operator*(bi a, bi b) {
+    short sign = -1;
+    if (signum(a) == signum(b) && signum(a) == -1) {
+        return -a * -b;
+    }
+    if (signum(a) == -1) {
+        return -(-a * b);
+    } 
+    if (signum(b) == -1) {
+        return -(a * -b);
+    }
+    bi ans = 0;
+
+    if (a < b) {
+        bi tmp = a; // if you will overload swap, change it
+        a = b;
+        b = tmp;
+    }
+
+    long long curr_power = 0;
+    short last = 0;
+    for (long long i = b.size() - 1; i >= 0; --i) {
+        bi temp = 0;
+        if (b.num[i] == '0') {
+            curr_power = curr_power + 1;
+            continue;
+        }
+
+        for (long long j = a.size() - 1; j >= 0; --j) {
+            short curr = (a.num[j] - '0') * (b.num[i] - '0');
+            temp = temp + curr * binpow(10, a.size() - j - 1);
+        }
+        temp.num += std::string(curr_power, '0');
+        ans = ans + temp;
+        curr_power = curr_power + 1;
+    }
+    if (last) {
+        ans = ans + (last * binpow(10, curr_power));
+    }
+    return ans;
+}
+
+bi operator*(bi a, auto b) {
+    return a * bi(b);
 }
 
 bi operator-(bi a, auto b) {
@@ -270,6 +327,16 @@ bi operator+(bi a, auto b) {
 
 bi operator+=(bi &a, auto b) {
     a = a + bi(b);
+    return a;
+}
+
+bi operator-=(bi &a, auto b) {
+    a = a - bi(b);
+    return a;
+}
+
+bi operator*=(bi &a, auto b) {
+    a = a * bi(b);
     return a;
 }
 
@@ -287,5 +354,13 @@ bi operator++(bi &a, int value) {
 namespace std {
     bi min(bi a, bi b) {
         return a < b ? a : b;
+    }
+    
+    bi max(bi a, bi b) {
+        return a > b ? a : b;
+    }
+
+    bi abs(bi a) {
+        return a < 0 ? -a : a;
     }
 }
