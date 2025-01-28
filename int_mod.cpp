51d7a7b111a01269877bf88cf21d88532d855676
Nikituk <karpikov.nv@ftl.name>
2025-01-28 11:34:30 +0300

    Create int_mod.cpp

---
+++ int_mod.cpp
@@ -0,0 +1,61 @@
+class int_mod {
+	ll value;
+	const ll mod;
+
+ public:
+	int_mod() : value(0), mod(1e9 + 7) {}
+
+	int_mod(ll a) : mod(1e9 + 7) {
+		if (a >= mod) a %= mod;
+		value = a;
+	}
+
+	int_mod operator+(int_mod other) {
+		ll res = this->value + other.value;
+		if (res >= mod) res -= mod;
+		return res;
+	}
+
+	int_mod operator-(int_mod other) {
+		ll res = this->value - other.value;
+		if (res < 0) res += mod;
+		return res;
+	}
+
+	int_mod operator*(int_mod other) {
+		long long res = 1LL * this->value * other.value;
+		if (res >= mod) {
+			res %= mod;
+		}
+		return res;
+	}
+
+	int_mod operator+(ll other) { return *this + int_mod(other); }
+
+	int_mod operator-(ll other) { return *this - int_mod(other); }
+
+	int_mod operator*(ll other) { return *this * int_mod(other); }
+
+	int_mod operator=(int_mod other) { return value = other.value; }
+
+	int_mod operator=(ll other) { return value = other; }
+
+	int_mod operator+=(int_mod other) { return *this = *this + other; }
+
+	int_mod operator-=(int_mod other) { return *this = *this - other; }
+
+	int_mod operator*=(int_mod other) { return *this = *this * other; }
+
+	explicit operator ll() { return value; }
+};
+
+std::istream& operator>>(std::istream& is, int_mod &imod) {
+	ll x;
+    is >> x;
+    imod = x;
+    return is;
+}
+
+std::ostream& operator<<(std::ostream& os, int_mod &imod) {
+	return os << (ll)imod;
+}
